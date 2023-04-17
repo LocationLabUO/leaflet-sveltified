5f2061c41e21d49fc1dbe4dca272f999de03b9d3
Andreas Wenzel <awenzel5@uoregon.edu>
2023-04-17 10:53:31 -0700

    export vector layers

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # leaflet-sveltified
 
 A library for working with leaflet in a svelte-y way. Inspired heavily, but completely rewritten from [svelte-leafletjs](https://www.npmjs.com/package/svelte-leafletjs).
+
+This package is: Fully Typescript and doesn't break SSR (everything handled on mount) in svelte-kit.
--- src/lib/layers/ui/index.ts
+++ src/lib/layers/ui/index.ts
@@ -1,9 +1,9 @@
 export * from './contexts';
 export * from './events';
 export { Marker, Tooltip, Popup, Icon, DivIcon };
+
 import DivIcon from './DivIcon.svelte';
 import Icon from './Icon.svelte';
 import Marker from './Marker.svelte';
-// import Marker from "./Marker.svelte"
 import Popup from './Popup.svelte';
 import Tooltip from './Tooltip.svelte';
